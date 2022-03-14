05dbc8d98472174b6c7fa059eb075c4b53b2ebc4
Esteban8482 <esteban.salazar@utp.edu.co>
2022-03-14 01:26:51 -0500

    Agregada la funcion 'popFront' junto a su documentacion, ademas fueron solucionados algunos errores descriptivos de otras funciones

--- Vector/PopFront/vector-pop_front.cc
+++ Vector/PopFront/vector-pop_front.cc
@@ -1,10 +1,10 @@
 /*
-A este archivo se le agregaron todas las funciones necesarias para
-una funcion que elimine elementos al final del vector
+A este archivo se le agregaron todas las funciones necesarias para 
+una funcion que elimine el elemento del final del vector en 
+cada ejecucion
 Contiene: 
--Funcion "popBack"
-*/
 
+*/
 #include <iostream>
 
 using namespace std;
@@ -36,6 +36,7 @@ private:
         storage = ns;
         capacity = capacity * 2;
     }
+    
 public:
 	
     void push_back(T elem) {
@@ -58,13 +59,22 @@ public:
 
     //INICIO CAMBIOS
     /*
-    Funcion que elimina el ultimo elemento de un vector, esto lo hace
-    sacando del conteo de elementos a el ultimo de estos, de esta
-    forma el ultimo elemento se volvera parte de la basura contenida
-    en la diferencia entre 'sz' y 'storage'
+    Funcion que elimina el primer elemento de un vector, esto lo hace 
+    moviendo hacia la izquierda todos los componentes del vector 
+    haciendo que eventualmente el primero salga del vector.
+    Tiene la desventaja que debe mover todos y cada uno de los
+    elementos del vector, por lo que si el vector es muy grande tal
+    vez de problemas de rendimiento
     */
-    void pop_back()
+    void pop_front()
     {
+        //Mover los componentes
+        for (int i = 1; i < sz; i++)
+        {
+            storage[i - 1] = storage[i];
+        }
+
+        //Informar sobre la reduccion del tamaño
         sz--;
     }
     //FIN CAMBIOS
@@ -77,9 +87,10 @@ int main() {
     {
     	a.push_back(4);
 	}
-
-    a.pop_back();
     a.print();
-    
+
+    a.pop_front();
+    a.print();
+
     return 0;
 }

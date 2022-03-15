e1352ba7b715a36be593da721fdae27766b2a62f
Esteban8482 <esteban.salazar@utp.edu.co>
2022-03-15 15:02:44 -0500

    Actualizadas las funciones iniciales en el archivo base de la lista

--- ListaEnlazada/Back/ListaEnlazada_Back.cc
+++ ListaEnlazada/Back/ListaEnlazada_Back.cc
@@ -2,11 +2,6 @@
 A este archivo se le agregaron todas las funciones necesarias para
 que retorne el dato dentro del ultimo nodo
 Contiene:
--Funcion "getNext"
--Funcion "getData"
--Destructor De Lista
--Funcion "size"
--Funcion "empty"
 -Funcion "back"
 */
 #include <iostream>
@@ -31,13 +26,11 @@ private:
             next = n; 
         }
 
-        //Funcion que retorna el siguiente nodo al que se esta ejecutando
         Node* getNext() 
         { 
             return next; 
         }
 
-        //Funcion que retorna el dato guardado en el nodo
         T getData ()
         {
             return data;
@@ -46,22 +39,13 @@ private:
     };
     Node* first;
     Node* last;
-
-    /*
-    Variable que emulara una especie de indexacion para posicionamiento
-    para recorrer los nodos con cierto orden numerico
-    */
     size_t sz;
 
 
 public:
 
-    List() : first(nullptr), last(nullptr), sz(0)
-    {
+    List() : first(nullptr), last(nullptr), sz(0) {}
 
-    }
-
-    //Destructor de la lista, funcion contraria al constructor
     ~List()
     {
         Node* n = first;
@@ -72,64 +56,36 @@ public:
         }
     }
 
-    /*
-    Funcion que de manera ciclica pasa por cada uno de los nodos para 
-    al final de la ejecucion saber cuantos elementos existen.
-
-    ATENCION: Ejecutar esta funcion puede conllevar a problemas de 
-    rendimiento debido a su recorrido por toda la lista
-    */
-    size_t size() {
-        //Variable de posicion
+    size_t size() 
+    {
         size_t s = 0;
 
-        //Nodo temporal para recorrer la lista
         Node* n = first;
 
-        /*
-        Recorrer la lista de modo que en cada salto de nodo se aumente
-        el conteo, el ciclo se detendra cuando no detecte mas elementos
-        en la lista
-        */
         while(n != nullptr) {
             s++;
             n = n->getNext();
         }
 
-        //Retorna el valor final del conteo
         return s;
     }
 
-    /*
-    Funcion que confirma si la lista esta vacia mediante el primer nodo
-    al revisar si este esta vacio puede saber si el resto de la lista
-    esta vacia
-    */
     bool empty() 
     { 
         return first == nullptr;
     }
 
-    /*
-    Se actualizo el funcionamiento del push back a una version mas eficiente
-    que realiza primero una comprobacion si esta tratando con una lista
-    vacia o que ya tiene elementos
-    */
     void push_back(T elem)
     {
-        //Creacion de nodo temporal
         Node* n = new Node(elem);
 
-        //Revisar si la lista esta vacia
         if (empty())
             first = n;
         else
             last->setNext(n);
 
-        //Cambiar la propiedad del ultimo elemento
         last = n;
 
-        //Actualizar el tamaño de la lista
         sz++;
     }
 
--- ListaEnlazada/Base/ListaEnlazada_Base.cc
+++ ListaEnlazada/Base/ListaEnlazada_Base.cc
@@ -43,6 +43,18 @@ class List {
 				{
 					next = n;
 				}
+
+				//Funcion que retorna el siguiente nodo al que se esta ejecutando
+        		Node* getNext() 
+        		{ 
+            	return next; 
+        		}
+
+				//Funcion que retorna el dato guardado en el nodo
+        		T getData ()
+        		{
+            		return data;
+				}
 		};
 
 		/*
@@ -51,26 +63,94 @@ class List {
 		*/
 		Node* first;
 		Node* last;
+
+		/*
+   	 	Variable que emulara una especie de indexacion para posicionamiento
+    	para recorrer los nodos con cierto orden numerico
+    	*/
+    	size_t sz;
 	public:
 
 	//Constructor de la lista que inicia sin ningun nodo en ella
 	List () : first(nullptr), last(nullptr) {
 	}
 	
+	//Destructor de la lista, funcion contraria al constructor
+    ~List()
+    {
+        Node* n = first;
+        while(n != nullptr) {
+            n = first;
+            first = first->getNext();
+            delete n;
+        }
+    }
+
 	/*
-	Funcion que agrega un nodo al final y configura el nuevo nodo como 'last'
-	*/
-	void push_back(T elem)
-	{
-		Node* x = new Node(elem);
-		last->setNext(x);
-		last = x;
-	} 
+    Funcion que de manera ciclica pasa por cada uno de los nodos para 
+    al final de la ejecucion saber cuantos elementos existen.
+
+    ATENCION: Ejecutar esta funcion puede conllevar a problemas de 
+    rendimiento debido a su recorrido por toda la lista
+    */
+    size_t size() {
+        //Variable de posicion
+        size_t s = 0;
+
+        //Nodo temporal para recorrer la lista
+        Node* n = first;
+
+        /*
+        Recorrer la lista de modo que en cada salto de nodo se aumente
+        el conteo, el ciclo se detendra cuando no detecte mas elementos
+        en la lista
+        */
+        while(n != nullptr) {
+            s++;
+            n = n->getNext();
+        }
+
+        //Retorna el valor final del conteo
+        return s;
+    }
+
+	/*
+    Funcion que confirma si la lista esta vacia mediante el primer nodo
+    al revisar si este esta vacio puede saber si el resto de la lista
+    esta vacia
+    */
+    bool empty() 
+    { 
+        return first == nullptr;
+    }
+
+	/*
+    Se actualizo el funcionamiento del push back a una version mas eficiente
+    que realiza primero una comprobacion si esta tratando con una lista
+    vacia o que ya tiene elementos
+    */
+    void push_back(T elem)
+    {
+        //Creacion de nodo temporal
+        Node* n = new Node(elem);
+
+        //Revisar si la lista esta vacia
+        if (empty())
+            first = n;
+        else
+            last->setNext(n);
+
+        //Cambiar la propiedad del ultimo elemento
+        last = n;
+
+        //Actualizar el tamaño de la lista
+        sz++;
+    }
 };
 
 int main()
 {
-	List<char> c;
+	List<int>* c = new List<int>();
 	
 	return 0;
 }
